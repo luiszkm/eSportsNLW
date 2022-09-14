dffdb32c8b3e3ef5f43f8bcaa6fde8a2f1df56dd
Luis Murilo <luiszkm@gmail.com>
2022-09-13 23:44:22 -0300

    add font inter and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './styles/main.css'
 import logo from './assets/Logo.svg'
+import image from './assets/game.png'
+
+import { MagnifyingGlassPlus } from "phosphor-react"
+
+
 function App() {
 
   return (
@@ -9,11 +14,56 @@ function App() {
         Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui
       </h1>
 
-      <div className='grid grid-cols-6 gap-6'>
-        <a href="#">
-          <img src="https://images.unsplash" alt="" />
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+
+        <a href="#" className=' relative rounded-lg overflow-hidden'>
+          <img src={image} alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 '>
+            <strong className='font-bold text-white block'>dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>anúncios</span>
+          </div>
         </a>
 
+        <a href="#" className=' relative rounded-lg overflow-hidden'>
+          <img src={image} alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 '>
+            <strong className='font-bold text-white block'>dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>anúncios</span>
+          </div>
+        </a>
+        <a href="#" className=' relative rounded-lg overflow-hidden'>
+          <img src={image} alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 '>
+            <strong className='font-bold text-white block'>dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>anúncios</span>
+          </div>
+        </a>
+        <a href="#" className=' relative rounded-lg overflow-hidden'>
+          <img src={image} alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 '>
+            <strong className='font-bold text-white block'>dota 2</strong>
+            <span className='text-zinc-300 text-sm block '>anúncios</span>
+          </div>
+        </a>
+
+
+      </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex  items-center  justify-between'>
+          <div>
+            <strong className='text-2xl text-white block font-black'>Não encontrou seu duo?</strong>
+            < span className='text-zinc-300 text block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
 
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,15 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily:{
+      sans:['inter','sans']
+    },
     extend: {
       backgroundImage:{
         galaxy: "url('/Bg.png')",
-
-        "nlw-gradient": 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)'
+        "nlw-gradient": ' linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        
+        "game-gradient": 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
       colors: {
       
